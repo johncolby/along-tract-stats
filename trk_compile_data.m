127b7b7b5787886887b11049c16bb43b33e9a339
John Colby <johncolby@ucla.edu>
2011-01-20 09:08:23 -0800

    Adaptive number of interpolation points
    In trk_compile_data.m, you can now automatically choose the number of interpolation points based on the tract lengths of the first subject. This is determined by a new 'nPts' column in tract_info.txt that takes the place of the old nPts input argument.
    
    If nPts is empty ([]) for a given tract, trk_compile_data.m will determine the # of interpolation points based on the mean streamline length of the first subject. This number will then be used for the rest of the subjects.
    
    Since interpolated tracks can now have differing numbers of vertices, track_means and scalar_means are now combined in a structure form instead of being matrices.
    
    Exchanged the for loops over subjects and tracts to make this easier. Also allowed the QC fig printing to be brought inside the loops.

--- trk_compile_data.m
+++ trk_compile_data.m
@@ -1,39 +1,38 @@
-function [track_means,scalar_means,starting_pts_out] = trk_compile_data(subsDir,subIDs,tract_info,nPts,outDir,starting_pts_in)
+function [track_means,starting_pts_out] = trk_compile_data(subsDir,subIDs,tract_info,outDir,starting_pts_in)
 %TRK_COMPILE_DATA - Compiles along-tract data for subjects/hemispheres/tracts
-%This function performs the along-tract
 %
-% Syntax: [track_means,scalar_means,starting_pts_out] = trk_compile_data(subsDir,subIDs,tract_info,nPts,outDir,starting_pts_in)
+% Syntax: [track_means,starting_pts_out] = trk_compile_data(subsDir,subIDs,tract_info,outDir,starting_pts_in)
 %
 % Inputs:
 %    subsDir    - Path to subject directory [char]
-%    subIDs     - List of subject ID folders in subsDir [n x 1 cell or num array]
+%    subIDs     - List of subject ID folders in subsDir [nSubs x 1 cell or num array]
 %    tract_info - Dataset with tract names, default origins, and viewpoints
-%    nPts       - Number of points for tract interpolation (Default: 30)
 %    outDir     - Path to output directory. (Default: PWD)
 %    starting_pts_in - Dataset with tract origins. Useful for reusing past
 %        origins that were determined interactively
 %
 % Outputs:
-%    track_means      - 4D matrix of mean tract geometries [nPts x 3 x nTracts x 
-%        nSubjects]
-%    scalar_means     - 3D matrix of tract scalars [nPts x nTracts x nSubjects]
-%    starting_pts_out - Tract origins used for flipping. Possibly a combination
-%        of defaults stored in the tract_info dataset, and interactively chosen
-%        ones.
+%    track_means      - Structure array with mean tract geometries and along-tract
+%    scalar mean and standard deviation estimates.
+%    starting_pts_out - Dataset with tract origins used for flipping. Possibly a
+%        combination of defaults stored in the tract_info dataset, and
+%        interactively chosen ones.
 %
 % Output files:
 %    starting_pts_out.txt - Same as above
 %    Tracking_QC_*.pdf - Quality control images for each tract. Allows you to
 %        quickly see if the flipping/interpolating appears successful.
-%    trk_data.txt - 
-%    trk_props_long.txt - 
+%    trk_data.txt - Data table with scalar means and standard deviations for
+%        each subject/tract/hemisphere (for stats in R).
+%    trk_props_long.txt - Data table with number of streamlines for each
+%        subject/tract/hemisphere (for stats in R).
 %
-% Example: 
+% Example:
 %    subsDir         = exDir;
 %    subIDs          = {'subject1'};
 %    tract_info      = dataset('file', fullfile(exDir, 'tract_info.txt'));
 %    starting_pts_in = dataset('file', fullfile(exDir, 'starting_pts_out.txt'));
-%    [track_means,scalar_means,starting_pts_out] = trk_compile_data(subsDir,subIDs,tract_info,[],[],starting_pts_in);
+%    [track_means,scalar_means,starting_pts_out] = trk_compile_data(subsDir,subIDs,tract_info,[],starting_pts_in);
 %
 % Other m-files required: read_avw, trk_read, trk_interp, trk_flip, trk_restruc,
 % trk_add_sc, trk_mean_sc, trk_plot, dataset, export
@@ -47,13 +46,12 @@ function [track_means,scalar_means,starting_pts_out] = trk_compile_data(subsDir,
 % Sept 2010
 
 %% Check and format input arguments
-if nargin < 6 || isempty(starting_pts_in)
+if nargin < 5 || isempty(starting_pts_in)
     starting_pts_in = dataset();
 elseif ~iscell(starting_pts_in.Subject) % Reformat to cells if subIDs are all numeric
     starting_pts_in.Subject = cellfun(@num2str, num2cell(starting_pts_in.Subject), 'UniformOutput', 0);
 end
-if nargin < 5 || isempty(outDir), outDir = pwd; end
-if nargin < 4 || isempty(nPts), nPts = 30; end
+if nargin < 4 || isempty(outDir), outDir = pwd; end
 if nargin < 3, error('Need more input arguments'), end
 if isnumeric(subIDs), subIDs = num2cell(subIDs); end
 
@@ -72,35 +70,46 @@ fprintf(fid2, 'ID\tPoint\tHemisphere\tTract\tFA\tSD');
 % Initialize QC figures
 for iFig=1:length(tract_info), fh(iFig) = figure; hold on; end
 
-% Initialize storage variables
-track_means      = zeros(nPts, 3, length(tract_info), length(subIDs));
-scalar_means     = zeros(nPts, length(tract_info), length(subIDs));
+% Initialize variables
+track_means = struct([]);
 starting_pts_out = dataset();
 
 %% Main loop to extract along-tract properties
-% Loop over subjects
-for i=1:length(subIDs)
-    subStr  = num2str(subIDs{i});
+% Loop over tracks
+for iTrk=1:length(tract_info)
+    nPts_set = 0;
     
-    % Load scalar volume
-    % Note: Modify path according to your directory setup
-    volPath = fullfile(subsDir, subStr, 'dti_fa.nii.gz');
-    volume  = read_avw(volPath);
-    
-    % Loop over tracks
-    for iTrk=1:length(tract_info)
+    % Loop over subjects
+    for i=1:length(subIDs)
         pt_start = [];
+        
         try
+            % Load scalar volume
+            % Note: Modify path according to your directory setup
+            subStr  = num2str(subIDs{i});
+            volPath = fullfile(subsDir, subStr, 'dti_fa.nii.gz');
+            volume  = read_avw(volPath);
+            
             % Load tract group
             % Note: Modify path according to your directory setup
             trkFile = sprintf('%s_%s.trk', tract_info.Tract{iTrk}, tract_info.Hemisphere{iTrk});
             trkPath = fullfile(subsDir, subStr, trkFile);
             [header tracks] = trk_read(trkPath);
             
+            % Determine # of interpolation points
+            if nPts_set == 0
+                if isnan(tract_info.nPts(iTrk))
+                    nPts = round(mean(trk_length(tracks))/header.voxel_size(1));
+                else
+                    nPts = tract_info.nPts(iTrk);
+                end
+                nPts_set = 1;
+            end
+            
             % Interpolate streamlines
             tracks_interp = trk_interp(tracks, nPts);
             
-            % Determine 'starting_pt' near tract origin. First look in
+            % Determine 'pt_start' near tract origin. First look in
             % 'starting_pts_in' if available, and then the 'tract_info' defaults
             if ~isempty(starting_pts_in)
                 pt_start = double(starting_pts_in(strcmp(subStr, starting_pts_in.Subject) &...
@@ -114,48 +123,49 @@ for i=1:length(subIDs)
             % Reorient streamlines according to 'pt_start'. Determine
             % interactively if needed
             [tracks_interp pt_start] = trk_flip(header, tracks_interp, pt_start, volume);
-            starting_pts_out = [starting_pts_out; dataset({subStr}, tract_info.Hemisphere(iTrk), tract_info.Tract(iTrk), pt_start,...
-                'VarNames',{'Subject','Hemisphere','Tract','Point'})];
+            starting_pts_out = [starting_pts_out; dataset({subStr}, tract_info.Hemisphere(iTrk), tract_info.Tract(iTrk), pt_start(1), pt_start(2), pt_start(3),...
+                'VarNames',{'Subject','Hemisphere','Tract','PointX','PointY','PointZ'})];
             tracks_interp_str = trk_restruc(tracks_interp);
             
             % Extract scalar values from 'volume'
             [header_sc tracks_sc] = trk_add_sc(header,tracks_interp_str,volume,'FA');
             
             % Determine the mean scalar at each cross section along the tract group
-            [scalar_means(:,iTrk,i) scalar_sd(:,iTrk,i)] = trk_mean_sc(header_sc,tracks_sc);
-            
-            % Determine the mean streamline geometry for display in QC figures
-            track_means(:,:,iTrk,i) = mean(tracks_interp, 3);
+            [scalar_mean scalar_sd] = trk_mean_sc(header_sc,tracks_sc);
             
             % Write outputs
             fprintf(fid1, '\n%s\t%s\t%s\t%d', subStr, tract_info.Hemisphere{iTrk}, tract_info.Tract{iTrk}, header.n_count);
             for iPt=1:nPts
-                fprintf(fid2, '\n%s\t%d\t%s\t%s\t%0.4f\t%0.4f', subStr, iPt, tract_info.Hemisphere{iTrk}, tract_info.Tract{iTrk}, scalar_means(iPt,iTrk,i), scalar_sd(iPt,iTrk,i));
+                fprintf(fid2, '\n%s\t%d\t%s\t%s\t%0.4f\t%0.4f', subStr, iPt, tract_info.Hemisphere{iTrk}, tract_info.Tract{iTrk}, scalar_mean(iPt), scalar_sd(iPt));
             end
+            
+            % Determine the mean streamline geometry for display in QC figures
+            track_mean = mean(tracks_interp, 3);
+            track_mean_sc_str = trk_restruc([track_mean scalar_mean scalar_sd]);
+            
+            % Draw QC figure
+            figure
+            subplot(5,5,i)
+            trk_plot(header, track_mean_sc_str, volume, [48 48 4])
+            view(tract_info.view(iTrk,:))
+            title(subStr)
+            axis off
+            
+            % Save QC figure
+            set(gcf, 'PaperSize', [10.5 8])
+            set(gcf, 'PaperPosition', [0 0 10.5 8])
+            trkName = sprintf('%s_%s', tract_info.Tract{iTrk}, tract_info.Hemisphere{iTrk});
+            print(gcf, '-dpdf', fullfile(outDir, sprintf('Tracking_QC_%s.pdf', trkName)), '-r300')
+            
         catch me % No streamlines
             fprintf(fid1, '\n%s\t%s\t%s\t0', subStr, tract_info.Hemisphere{iTrk}, tract_info.Tract{iTrk});
-            fprintf('Failed to read subject %s %s\n', subStr, trkFile)
+            fprintf('Failed to process subject %s %s\n', subStr, trkFile)
             warning(me.message)
         end
-        
-        % Draw QC figures
-        figure(fh(iTrk))
-        subplot(5,5,i)
-        trk_plot(header, trk_restruc(track_means(:,:,iTrk,i)), volume, [48 48 4])
-        view(tract_info.view(iTrk,:))
-        title(subStr)
-        axis off
     end
 end
 
-%% Save files and clean up
-for iFig=1:length(tract_info)
-    figure(fh(iFig))
-    set(gcf, 'PaperSize', [10.5 8])
-    set(gcf, 'PaperPosition', [0 0 10.5 8])
-    print(gcf, '-dpdf', fullfile(outDir, sprintf('Tracking_QC_%s.pdf', trkFile)), '-r300')
-end
-
+%% Clean up
 export(starting_pts_out, 'file', fullfile(outDir, 'starting_pts_out.txt'))
 
 close all
