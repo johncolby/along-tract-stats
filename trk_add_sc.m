6dbebf6fb6eb943f268f4bb3181620a33762d1dc
John Colby <johncolby@ucla.edu>
2012-09-18 14:26:13 -0700

    trk_add_sc now can attach a single scalar
    Now also handles vertices that fall outside the MRI volume. These would cause errors before, but are now returned as NaNs so will be ignored.

--- trk_add_sc.m
+++ trk_add_sc.m
@@ -1,4 +1,4 @@
-function [header,tracks] = trk_add_sc(header,tracks,volume,name)
+function [header,tracks] = trk_add_sc(header,tracks,source,name)
 %TRK_ADD_SC - Attaches a scalar value to each vertex in a .trk track group
 %For example, this function can look in an FA volume, and attach the
 %corresponding voxel FA value to each streamline vertex.
@@ -8,7 +8,8 @@ function [header,tracks] = trk_add_sc(header,tracks,volume,name)
 % Inputs:
 %    header - Header information from .trk file [struc]
 %    tracks - Track data struc array [1 x nTracks]
-%    volume - Scalar MRI volume
+%    source - Scalar MRI volume [3d array]. Alternatively, a single scalar to 
+%             attach to all vertices.
 %    name   - Description of the scalar to add to the header (e.g. 'FA')
 %
 % Outputs:
@@ -40,12 +41,23 @@ function [header,tracks] = trk_add_sc(header,tracks,volume,name)
 
 % Loop over # of tracks (slow...any faster way?)
 for iTrk=1:length(tracks)
-    % Translate continuous vertex coordinates into discrete voxel coordinates
-    vox = ceil(tracks(iTrk).matrix(:,1:3) ./ repmat(header.voxel_size, tracks(iTrk).nPoints,1));
-    
-    % Index into volume to extract scalar values
-    inds                = sub2ind(header.dim, vox(:,1), vox(:,2), vox(:,3));
-    scalars             = volume(inds);
+    if length(source) == 1
+        % Assign a whole-tract property as a vertex-wise scalar
+        scalars = repmat(tracks(iTrk).props(source), size(tracks(iTrk).matrix, 1), 1);
+    else
+        % Translate continuous vertex coordinates into discrete voxel coordinates
+        vox = ceil(tracks(iTrk).matrix(:,1:3) ./ repmat(header.voxel_size, tracks(iTrk).nPoints,1));
+        
+        % Find vertices that don't map into the volume
+        mask = ~logical(sum(vox < 1 | bsxfun(@minus, vox, header.dim) > 0, 2));
+        
+        % Index into volume to extract scalar values
+        inds          = sub2ind(header.dim, vox(mask,1), vox(mask,2), vox(mask,3));
+        scalars       = repmat(NaN, size(tracks(iTrk).matrix, 1), 1);
+        scalars(mask) = source(inds);
+        
+    end
+        
     tracks(iTrk).matrix = [tracks(iTrk).matrix, scalars];
 end
 
